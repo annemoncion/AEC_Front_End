66268d929ad062d5e92d86b3a4b90b67c86c602c
annemoncion <anne.m@hotmail.ca>
2019-11-15 09:07:13 -0500

    Complété exercice 2.10 sur le programme de couleurs - chapitre 2

--- Programmation1/Chapitre2/js/exercice2-10.js
+++ Programmation1/Chapitre2/js/exercice2-10.js
@@ -1,23 +1,34 @@
 // Créez un programme permettant de changer la couleur du texte de la console.
 // Offrez trois choix de couleurs de fond et de texte à l'utilisateur
 // Affichez une ligne de texte de la couleur de fond et de texte choisis par l'utilisateur
+var backgroundColor = "white";
+var color = "black";
 var couleurFond = prompt("Entrez une couleur de fond. Vous avez les trois choix suivants: bleu, vert ou rouge.")
 var couleurTexte = prompt("Entrez une couleur de texte. Vous avez les trois choix suivants: jaune, rose ou gris.");
+var erreurFond = "Attention. Les trois couleurs de fond disponibles sont 'bleu', 'vert' et 'rouge'.";
+var erreurTexte = "Attention. Les trois couleurs de texte disponibles sont 'jaune', 'rose' et 'gris'.";
 
-if ( couleurFond.toUpperCase() === "BLEU" ) {
-
+//Déterminer la couleur du fond.
+if ( couleurFond.toUpperCase() === "BLEU" || couleurFond.toUpperCase() === "BLUE" ) {
+    backgroundColor = "blue";
+} else if ( couleurFond.toUpperCase() === "VERT" || couleurFond.toUpperCase() === "GREEN" ) {
+    backgroundColor = "green";
+} else if ( couleurFond.toUpperCase() === "ROUGE" || couleurFond.toUpperCase() === "RED" ) {
+    backgroundColor = "red";
+} else {
+    document.write(erreurFond);
 }
 
-/*
-document.write("Cliquez sur la couleur de fond de votre choix:")
-document.write("<div id='fond1' style='background-color:" + couleur1 + "; height: 40px; width: 40px; margin: 20px;'></div>" );
-document.write("<div id='fond2' style='background-color:" + couleur2 + "; height: 40px; width: 40px; margin: 20px'></div>" );
-document.write("<div id='fond3' style='background-color:" + couleur3 + "; height: 40px; width: 40px; margin: 20px'></div>" );
+//Déterminer la couleur du texte.
+if ( couleurTexte.toUpperCase() === "JAUNE" || couleurTexte.toUpperCase() === "YELLOW" ) {
+    color = "yellow";
+} else if ( couleurTexte.toUpperCase() === "ROSE" || couleurTexte.toUpperCase() === "PINK" ) {
+    color = "pink";
+} else if ( couleurTexte.toUpperCase() === "GRIS" || couleurTexte.toUpperCase() === "GREY" ) {
+    color = "grey";
+} else {
+    document.write(erreurTexte);
+}
 
-document.write("Cliquez sur la couleur de texte de votre choix:")
-document.write("<div id='fond1' style='background-color:" + couleur1 + "; height: 40px; width: 40px; margin: 20px;'></div>" );
-document.write("<div id='fond2' style='background-color:" + couleur2 + "; height: 40px; width: 40px; margin: 20px'></div>" );
-document.write("<div id='fond3' style='background-color:" + couleur3 + "; height: 40px; width: 40px; margin: 20px'></div>" );
- */
-
-document.write("<div style='background-color:" + fond + "; color:" + texte + "; padding: 40px;'>Exemple de texte !</div>");
+//Exemple de texte avec les couleurs choisies.
+document.write("<div style='background-color:" + backgroundColor + "; color:" + color + "; font-size: 2em; padding: 40px;'>Exemple de texte !</div>");
