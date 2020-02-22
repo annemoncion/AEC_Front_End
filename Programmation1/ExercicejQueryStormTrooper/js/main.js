77e843857e79013a9739cad95cf401ae0bc5c252
annemoncion <anne.m@hotmail.ca>
2020-02-22 10:11:06 -0500

    Exercice Jumbotron modal video background

---
+++ Programmation1/ExercicejQueryStormTrooper/js/main.js
@@ -0,0 +1,12 @@
+$('#stormBlanc').attr('title', 'Pas facile la vie de Stormtrooper');
+$('#stormBlanc').attr("alt", "Image d'un stormtrooper blanc");
+$('#stormBlanc').attr("src", "images/nouveauStorm.jpeg");
+$('#fontAChanger').attr("class", "fas fa-fighter-jet");
+$('.row4 h1').css("font-size", "4rem");
+$(".orange").css("opacity", ".8");
+$(".fondNoir").css("background-color", "#111");
+$(".fondBlanc").css("background-color", "#999");
+$("#titrePage").html("Vive les Stormtroopers!");
+$("#sousDarthVader").html("Père de Luke Skywalker.");
+$("#aCacher").hide();
+
