2ff6286e3eefd2c8e8eeba3ac7abff961c1ec456
annemoncion <anne.m@hotmail.ca>
2020-04-28 20:12:57 -0400

    becflix build

---
+++ Programmation2/becflix/build/precache-manifest.ef2a057529648aa7cda3339c373612c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a67c388af3f49f176bc4de2ea947055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f7ad277042f3d0b5b5e",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d9513fdfa676b0b2f0e4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5f7ad277042f3d0b5b5e",
+    "url": "/static/js/2.2708b7df.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.2708b7df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9513fdfa676b0b2f0e4",
+    "url": "/static/js/main.6cc3bb33.chunk.js"
+  },
+  {
+    "revision": "c8cec117673065e84ca5",
+    "url": "/static/js/runtime-main.2b12ed2f.js"
+  },
+  {
+    "revision": "d60ec724e51bae34ce8355cfe4e61549",
+    "url": "/static/media/lotr1.d60ec724.jpg"
+  }
+]);
