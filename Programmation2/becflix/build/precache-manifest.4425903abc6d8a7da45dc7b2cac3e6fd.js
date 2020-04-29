9a5879cac1fff81ce923adf2acedcdeb5c51ddc0
annemoncion <anne.m@hotmail.ca>
2020-04-28 20:53:45 -0400

    run build becflix car files missing

---
+++ Programmation2/becflix/build/precache-manifest.4425903abc6d8a7da45dc7b2cac3e6fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7191f713bd6d1eb67163c245d5b95c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5f7ad277042f3d0b5b5e",
+    "url": "./static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "7b04ef1bb004af604bbd",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5f7ad277042f3d0b5b5e",
+    "url": "./static/js/2.2708b7df.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.2708b7df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b04ef1bb004af604bbd",
+    "url": "./static/js/main.a8413271.chunk.js"
+  },
+  {
+    "revision": "ef8e272eb776f0edacee",
+    "url": "./static/js/runtime-main.03e9e90d.js"
+  },
+  {
+    "revision": "d60ec724e51bae34ce8355cfe4e61549",
+    "url": "./static/media/lotr1.d60ec724.jpg"
+  }
+]);
