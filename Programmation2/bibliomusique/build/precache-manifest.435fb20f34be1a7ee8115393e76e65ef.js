517f3b626f027df22605cc7750908aa7e67e1704
annemoncion <anne.m@hotmail.ca>
2020-06-06 21:50:32 -0400

    biblio musique build

---
+++ Programmation2/bibliomusique/build/precache-manifest.435fb20f34be1a7ee8115393e76e65ef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc41339842364ada883e1d59fe60faa4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b52601dd722443862bbc",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f0cf4f26acaeae930280",
+    "url": "./static/css/main.11b2d9be.chunk.css"
+  },
+  {
+    "revision": "b52601dd722443862bbc",
+    "url": "./static/js/2.b8bcdd8a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.b8bcdd8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0cf4f26acaeae930280",
+    "url": "./static/js/main.d1f5f561.chunk.js"
+  },
+  {
+    "revision": "90893d693de58caf12bd",
+    "url": "./static/js/runtime-main.5fb2a573.js"
+  },
+  {
+    "revision": "df3d2c13035616e0ec1e66ecd0b7cc01",
+    "url": "./static/media/les-ombres-longues.df3d2c13.jpg"
+  },
+  {
+    "revision": "72f1027badafe43d6bfe88a79b842bf0",
+    "url": "./static/media/letoile-thoracique.72f1027b.jpg"
+  },
+  {
+    "revision": "6f947ea16c9148708d9b3b7d3c9244c2",
+    "url": "./static/media/logo.6f947ea1.svg"
+  },
+  {
+    "revision": "ce0dafa5539eb73337142f21f6c65c55",
+    "url": "./static/media/premier-juin.ce0dafa5.jpg"
+  },
+  {
+    "revision": "c8dd28af5cc0856d4c33ea6f6ec83b76",
+    "url": "./static/media/rencontrer-looloo.c8dd28af.jpg"
+  },
+  {
+    "revision": "f797fe1b48be5f7e5887a5db92c9f8d4",
+    "url": "./static/media/tu-ne-mourras-pas.f797fe1b.jpg"
+  },
+  {
+    "revision": "7c7b19dcd499c268e3d1d4278e47bfa6",
+    "url": "./static/media/ultramarr.7c7b19dc.jpg"
+  }
+]);
