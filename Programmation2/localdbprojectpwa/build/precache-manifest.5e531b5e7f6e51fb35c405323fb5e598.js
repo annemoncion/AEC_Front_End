e532e594096dc98c397a3215f7bbfbb308824d70
annemoncion <anne.m@hotmail.ca>
2020-08-25 19:08:42 -0400

    mise en ligne pwa test

---
+++ Programmation2/localdbprojectpwa/build/precache-manifest.5e531b5e7f6e51fb35c405323fb5e598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a85b5da75485bf0f1436b39775cbdcd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b012f0d7ebec5c626254",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bda11bef1b82ba816e36",
+    "url": "./static/js/2.ae539a19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ae539a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b012f0d7ebec5c626254",
+    "url": "./static/js/main.f5b6ccf9.chunk.js"
+  },
+  {
+    "revision": "bb4922d3edace0c405c7",
+    "url": "./static/js/runtime-main.11fb3d3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
