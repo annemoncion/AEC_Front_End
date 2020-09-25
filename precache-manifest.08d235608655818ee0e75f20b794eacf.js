a3526ea85ead00a8e59958626652fdef8abb12da
annemoncion <anne.m@hotmail.ca>
2020-09-25 15:48:08 -0400

    Updates

---
+++ precache-manifest.08d235608655818ee0e75f20b794eacf.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5924c1b87c52d6dad2b77c49b56176c7",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/index.html"
+  },
+  {
+    "revision": "73520caabf7a43ce81b6",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "62e900a70fcc875c0725",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/css/main.35da3937.chunk.css"
+  },
+  {
+    "revision": "73520caabf7a43ce81b6",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/js/2.026b398e.chunk.js"
+  },
+  {
+    "revision": "d77c619ca2e803ff4203c09914a159e3",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/js/2.026b398e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62e900a70fcc875c0725",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/js/main.5b2010f0.chunk.js"
+  },
+  {
+    "revision": "6bf66a986efb5d86ed7a",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/js/runtime-main.cc30a579.js"
+  },
+  {
+    "revision": "0cb8cbc79b388319f41f57c4770ac503",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/Metropolis-Bold.0cb8cbc7.woff2"
+  },
+  {
+    "revision": "ebbc53c17409161d3b84e406fd415343",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/Metropolis-Bold.ebbc53c1.woff"
+  },
+  {
+    "revision": "02ae4a8c8c0cf4cc4462e9053fedb024",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/Metropolis-BoldItalic.02ae4a8c.woff"
+  },
+  {
+    "revision": "b4be56d46aa39f564e88822d605236dd",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/Metropolis-BoldItalic.b4be56d4.woff2"
+  },
+  {
+    "revision": "36eb0bcd15e91a72c5250535446acfb5",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/Metropolis-Medium.36eb0bcd.woff"
+  },
+  {
+    "revision": "55005447b660f8538dc76765b94b58cc",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/Metropolis-Medium.55005447.woff2"
+  },
+  {
+    "revision": "09ec7bed6a9250d7081e3c70e07a8094",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/Metropolis-MediumItalic.09ec7bed.woff2"
+  },
+  {
+    "revision": "b6d038137faba22a53c8b4ee0738d379",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/Metropolis-MediumItalic.b6d03813.woff"
+  },
+  {
+    "revision": "1802be3431f3f92f6c4833c1db878c03",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/Metropolis-Regular.1802be34.woff"
+  },
+  {
+    "revision": "7a2b8b15453bfc7663c6425f3bfb5956",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/Metropolis-Regular.7a2b8b15.woff2"
+  },
+  {
+    "revision": "03f7451ede83f20331bfd486711dc8fd",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/Metropolis-RegularItalic.03f7451e.woff"
+  },
+  {
+    "revision": "5656fdd396e301e41f60d5e590bd71a6",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/Metropolis-RegularItalic.5656fdd3.woff2"
+  },
+  {
+    "revision": "c519970bc52b40e8e89abefcf0ab7c84",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/fond_plantes3.c519970b.jpg"
+  },
+  {
+    "revision": "97d7cb6a82592e8cd646be10469dfa65",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/icone-magie.97d7cb6a.svg"
+  },
+  {
+    "revision": "6bb1fa534328e36bc0e183a037e49e2f",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/icone-poubelle.6bb1fa53.svg"
+  },
+  {
+    "revision": "509c3689c5237583128b0a6e3b4c7340",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/icone-retour.509c3689.svg"
+  },
+  {
+    "revision": "2ef166bee332b5f66206a51e16d3565e",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/logo_annemoncion_illustration.2ef166be.svg"
+  },
+  {
+    "revision": "d3fde7f19dd9cd13bc649633a7aa4530",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/triangles-en-file.d3fde7f1.svg"
+  },
+  {
+    "revision": "db262d433025de0d0f2fbde9a30c9fb0",
+    "url": "/AEC_Front_End/Programmation2/portfolio-illustrations/static/media/vide.db262d43.svg"
+  }
+]);
